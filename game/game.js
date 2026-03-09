9011e75fdb842816ecc2e2a7226b79d3bfc32c0e
Jordan Mantel <jmantel@my.waketech.edu>
2026-03-09 14:06:59 -0400

    Misc. Changes

--- game/game.js
+++ game/game.js
@@ -1,10 +1,17 @@
 function loadChart(chartID) {
 
 }
+function drawText(font, content, x, y) {
+    // Note: Font should be "Sizepx FontName". Ex.: "30px Arial" "15px Verdana"
+    const C = document.getElementById("canvas");
+    const CTX = C.getContext("2d");
+    CTX.font = font;
+    CTX.fillText(content, x, y)
+}
 
 function gameLoop() {
     function renderUI() {
-
+        
     }
 
     function renderNotes() {
