3ac26af25acd41095d1cfd34a449e9a4c09adb58
Jordan Mantel <jmantel@my.waketech.edu>
2026-03-12 13:56:30 -0400

    Tried and failed to implement Fetch to retrieve JSONs

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let time = 1800;
+
 function calcTime() {
     hours = Math.floor(time / 3600);
     minutes = Math.floor((time - (hours * 3600)) / 60);   // This code borrowed from Geeks For Geeks & modified
@@ -33,14 +34,30 @@ function loadStory(scene) {
             throw new Error(`Could not retrieve story file due to an error: ${response.status}`);
         }
         return response.json();
-    }) .then(data => console.log(data))
+    }) .then(data => {console.log(data); return(data)})
     .catch(error => console.error("Failed to fetch data:", error));
-
-    // Apply story content to the page
-    let IMAGE = document.getElementById("image"); 
-    let MAINTEXT = document.getElementById("text");
-    let RECOMMEND = document.getElementById("recommendation"); // Provides a hint, e.g. when the puzzle of an area is solved, display a hint saying that there's nothing left to do here
-    let CHOICES = document.getElementById("choices");
+    
 }
 
-loadStory("central")
+function applyStoryContent(story) {
+    console.log(story);
+    storyData = JSON.parse(story);
+    // Apply story content to the page
+    const IMAGE = document.getElementById("image"); 
+    const MAINTEXT = document.getElementById("text");
+    const RECOMMEND = document.getElementById("recommendation"); // Provides a hint, e.g. when the puzzle of an area is solved, display a hint saying that there's nothing left to do here
+    const CHOICES = document.getElementById("choices");
+
+    IMAGE.src = storyData.image;
+    MAINTEXT.innerText = storyData.bodyText;
+    if ("recommend" in storyData) {
+        RECOMMEND.innerText = storyData.recommend;
+    }
+    CHOICES.innerHTML = "<ul>";
+    for (let i = 0; i < storyData.choices.length; i++) {
+        CHOICES.innerHTML += "<li>";
+        CHOICES.innerHTML += "<button onclick = 'applyStoryContent(loadStory(" + storyData.choices.keys[i] + "))'></li>";  
+    }
+}
+
+applyStoryContent(loadStory("central"));
